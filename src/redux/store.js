957c8f5ab0676f99cbfc562a198ceb9e68d5bb2c
Chris Vega <catalina@MacBook-Pro.local>
2020-08-13 14:18:29 -0700

    finished redux

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import reducer from './reducer';
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+
+
+
+
+
+
+export default createStore(reducer, applyMiddleware(promiseMiddleware));
